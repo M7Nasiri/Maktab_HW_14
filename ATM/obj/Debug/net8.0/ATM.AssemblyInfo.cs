d7726a3239ca6b106589f464d8e14211d59436d0
M7Nasiri <mo2001nasiri@gmail.com>
2025-10-07 11:29:17 +0330

    Remove bin and obj

--- ATM/obj/Debug/net8.0/ATM.AssemblyInfo.cs
+++ ATM/obj/Debug/net8.0/ATM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+804cfb97e0663117f4ae0b79bb3ca26636aabafa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ea27b0ad8de59f177cc8d7786f2a1bd2a5ac097")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
